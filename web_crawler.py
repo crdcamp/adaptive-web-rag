4f182faabd76766e59d54eeb6fa56f83b59dc29b
Christian Camp <crdcamp@gmail.com>
2026-05-22 00:25:13 -0400

    Very basic use of crawl4ai

---
+++ web_crawler.py
@@ -0,0 +1,11 @@
+# %% Imports
+from web_crawler import WebCrawler
+
+# Create an instance of WebCrawler
+crawler = WebCrawler()
+
+# Warm up the crawler (load necessary models)
+crawler.warmup()
+
+# Run the crawler on a URL
+result = crawler.run(url="https://openai.com/api/pricing/")
