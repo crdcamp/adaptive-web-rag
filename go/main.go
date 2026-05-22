454047c20d73fb204b892d3eec3b33bcf5a679f5
Christian Camp <crdcamp@gmail.com>
2026-05-22 19:52:56 -0400

    StartLLMServer function almost working

--- go/main.go
+++ go/main.go
@@ -8,22 +8,24 @@ import (
 var ChatModelPath string = "models/Qwen2.5-7B-Instruct-Q4_K_M.gguf"
 var ChatModelPort string = "8082"
 
-var EmbedModelPath string = "models/models/Qwen3-Embedding-8B-Q6_K.gguf"
+var EmbedModelPath string = "models/Qwen3-Embedding-8B-Q6_K.gguf"
 var EmbedModelPort string = "8081"
 
 func StartLLMServer(modelPath string, port string, embedding bool) {
 	var startServerCmd *exec.Cmd // Declare a variable of type *exec.Cmd
 
 	if !embedding {
-		startServerCmd = exec.Command("llama-server", "--model", chatModelPath, "--port", port)
+		startServerCmd = exec.Command("llama-server", "--model", ModelPath, "--port", port)
 	} else {
-		startServerCmd = exec.Command("llama-server", "--model", chatModelPath, "--port", port, "--embedding")
+		startServerCmd = exec.Command("llama-server", "--model", ModelPath, "--port", port, "--embedding")
 	}
 
 	if err := startServerCmd.Run(); err != nil {
 		log.Fatalf("Error: LLM server for model %s failed to start: %v", modelPath, err)
 	}
+}
 
 func main() {
-	StartLLMServer()
+	StartLLMServer(ChatModelPath, ChatModelPort, false)
+	StartLLMServer(EmbedModelPath, EmbedModelPort, true)
 }
