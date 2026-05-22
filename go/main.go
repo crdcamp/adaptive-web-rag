844e17edac45897e1d24a5ac527606338ef04357
Christian Camp <crdcamp@gmail.com>
2026-05-22 19:17:35 -0400

    Cleanup

--- go/main.go
+++ go/main.go
@@ -1,14 +1,27 @@
 package main
 
 import (
+	"log"
 	"os/exec"
 )
 
-// Initialize chat and embedding model servers
-func main() {
-	chatServerCmd, chatErr := exec.Command("llama-server", "--model", "models/Qwen2.5-7B-Instruct-Q4_K_M.gguf", "--port", "8082")
-	embedServerCmd, embedErr := exec.Command("llama-server", "--model", "models/Qwen3-Embedding-8B-Q6_K.gguf", "--port", "8081", "--embedding")
-	if chatErr || embedErr != nil {
-		panic(err)
+var chatModelPath string = "models/Qwen2.5-7B-Instruct-Q4_K_M.gguf"
+var embedModelPath string = "models/models/Qwen3-Embedding-8B-Q6_K.gguf"
+
+func startServers(chatModelPath string, chatPort string, embedModelPath string, embedPort string) {
+	// Initialize chat and embedding model servers
+	chatServerCmd := exec.Command("llama-server", "--model", chatModelPath, "--port", chatPort)
+	embedServerCmd := exec.Command("llama-server", "--model", embedModelPath, "--port", "8081", embedPort)
+
+	if err := chatServerCmd.Run(); err != nil {
+		log.Fatalf("chat server failed: %v", err)
 	}
+	if err := embedServerCmd.Run(); err != nil {
+		log.Fatalf("embed server failed: %v", err)
+	}
+
+}
+
+func main() {
+	startServers("8082", "8081")
 }
