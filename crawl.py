83c0ab4b6c13ae27af74fc34c9cb186116c9e601
Christian Camp <crdcamp@gmail.com>
2026-05-22 13:37:58 -0400

    New test crawl website

--- crawl.py
+++ crawl.py
@@ -2,12 +2,14 @@ import asyncio
 from crawl4ai import AsyncWebCrawler, CrawlerRunConfig
 from crawl4ai.markdown_generation_strategy import DefaultMarkdownGenerator
 
+# Find out if there's a chunk splitter designed for crawl4ai results
+
 async def main():
     config = CrawlerRunConfig(
         markdown_generator=DefaultMarkdownGenerator()
     )
     async with AsyncWebCrawler() as crawler:
-        result = await crawler.arun("https://medium.com/@filinvadim/llms-for-go-developers-a-plug-and-play-approach-with-llama-cpp-4ccccb6d04df", config=config)
+        result = await crawler.arun("https://github.com/crdcamp", config=config)
 
         if result.success:
             print("Raw Markdown Output:\n")
