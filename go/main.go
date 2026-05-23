cfb954f082a0353ec47a7e60407d7a983cf2bf2f
Christian Camp <crdcamp@gmail.com>
2026-05-22 20:09:09 -0400

    Test server working

--- go/main.go
+++ go/main.go
@@ -3,29 +3,39 @@ package main
 import (
 	"log"
 	"os/exec"
+	"time"
 )
 
+// Chat model params
 var ChatModelPath string = "models/Qwen2.5-7B-Instruct-Q4_K_M.gguf"
 var ChatModelPort string = "8082"
 
+// Embed model params
 var EmbedModelPath string = "models/Qwen3-Embedding-8B-Q6_K.gguf"
 var EmbedModelPort string = "8081"
 
-func StartLLMServer(modelPath string, port string, embedding bool) {
+func StartLLMServer(modelPath string, port string, embedding bool) *exec.Cmd {
 	var startServerCmd *exec.Cmd // Declare a variable of type *exec.Cmd
 
 	if !embedding {
-		startServerCmd = exec.Command("llama-server", "--model", ModelPath, "--port", port)
+		startServerCmd = exec.Command("llama-server", "--model", modelPath, "--port", port)
 	} else {
-		startServerCmd = exec.Command("llama-server", "--model", ModelPath, "--port", port, "--embedding")
+		startServerCmd = exec.Command("llama-server", "--model", modelPath, "--port", port, "--embedding")
 	}
 
-	if err := startServerCmd.Run(); err != nil {
+	if err := startServerCmd.Start(); err != nil {
 		log.Fatalf("Error: LLM server for model %s failed to start: %v", modelPath, err)
 	}
+	return startServerCmd
 }
 
 func main() {
-	StartLLMServer(ChatModelPath, ChatModelPort, false)
-	StartLLMServer(EmbedModelPath, EmbedModelPort, true)
+	chatCmd := StartLLMServer(ChatModelPath, ChatModelPort, false)
+	embedCmd := StartLLMServer(EmbedModelPath, EmbedModelPort, true)
+
+	// For testing: kill the server after one minute
+	time.Sleep(1 * time.Minute)
+
+	chatCmd.Process.Kill()
+	embedCmd.Process.Kill()
 }
--- start-llama-server.sh
+++ start-llama-server.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 
 # Start servers in background
-llama-server --model models/Qwen3-Embedding-8B-Q6_K.gguf --port 8081 --embedding &
 llama-server --model models/Qwen2.5-7B-Instruct-Q4_K_M.gguf --port 8082 &
+llama-server --model models/Qwen3-Embedding-8B-Q6_K.gguf --port 8081 --embedding &
 
 # Wait for them to be ready (they take a few seconds to load)
 sleep 10
