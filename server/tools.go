d277beee65eede23a7c430f90994f741d39478b0
Christian Camp <crdcamp@gmail.com>
2026-05-28 11:51:30 -0400

    Checkpoint

--- server/tools.go
+++ server/tools.go
@@ -17,18 +17,18 @@ func UnloadModel(modelName string) {
 	payload, _ := json.Marshal(map[string]string{"model": modelName})
 	resp, err := http.Post(unloadURL, "application/json", bytes.NewBuffer(payload))
 	if err != nil {
-		panic(err) // Probably need a different way to handle this error
+		panic(err) // Need a better error handling method here
 	}
 	defer resp.Body.Close()
 	fmt.Printf("Status: %s\n", resp.Status)
 }
 
-// Generate a search query to pass into `crawl.py`
-func GenerateSearchQueries(modelName string, userPrompt string) string {
+// Generate a search query to pass on to `crawl.py`
+func GenerateSearchQuery(modelName string, userPrompt string) string {
 	ctx := context.Background()
 	client := openai.NewClient(
 		option.WithBaseURL(ServerBaseURL),
-		option.WithAPIKey(APIKey), // Leaving this here just in case
+		option.WithAPIKey(APIKey), // No API in use currently, but leaving this here just in case
 	)
 	systemMessage := "You are a search query generator. When given a question or topic, generate a search engine query that a person could enter into a browser to research it."
 
@@ -37,7 +37,7 @@ func GenerateSearchQueries(modelName string, userPrompt string) string {
 			openai.SystemMessage(systemMessage),
 			openai.UserMessage(userPrompt),
 		},
-		Model: ChatModel,
+		Model: modelName,
 	})
 	if err != nil {
 		panic(err) // Need a better error handling method here
@@ -48,4 +48,6 @@ func GenerateSearchQueries(modelName string, userPrompt string) string {
 	return chatResponse
 }
 
-//func callCrawlScript() (Or something along those lines)
+// func SomethingAboutVectorDB() {}
+
+//func CallCrawlScript() {}
