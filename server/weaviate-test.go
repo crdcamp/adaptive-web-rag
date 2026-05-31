537a2c8a69135adfb1da78056bf56f115e1c051a
Christian Camp <crdcamp@gmail.com>
2026-05-30 21:16:06 -0600

    Weaviate test template and go modules

---
+++ server/weaviate-test.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"context"
+	"fmt"
+
+	"github.com/weaviate/weaviate-go-client/v5/weaviate"
+)
+
+func GetSchema() {
+	cfg := weaviate.Config{
+		Host:   "localhost:8080",
+		Scheme: "http",
+	}
+	client, err := weaviate.NewClient(cfg)
+	if err != nil {
+		panic(err)
+	}
+
+	schema, err := client.Schema().Getter().Do(context.Background())
+	if err != nil {
+		panic(err)
+	}
+	fmt.Printf("%v", schema)
+}
+
+func test() {
+	GetSchema()
+}
