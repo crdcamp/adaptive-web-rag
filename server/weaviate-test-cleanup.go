0e663ed6e1c89df408f60fec91ec5a6316d01bbd
Christian Camp <crdcamp@gmail.com>
2026-05-31 19:39:42 -0600

    Rewrote CreateCollection function in new file

--- server/weaviate-test-cleanup.go
+++ server/weaviate-test-cleanup.go
@@ -3,12 +3,15 @@ package main
 // Reference: https://docs.weaviate.io/weaviate/manage-collections/collection-operations
 
 import (
+	"context"
 	"fmt"
 
 	"github.com/weaviate/weaviate-go-client/v5/weaviate"
+	"github.com/weaviate/weaviate/entities/models"
+	"github.com/weaviate/weaviate/entities/schema"
 )
 
-func CleanTest() {
+func WeaviateTest() {
 	cfg := weaviate.Config{
 		Host:   "localhost:8080",
 		Scheme: "http",
@@ -22,11 +25,40 @@ func CleanTest() {
 	fmt.Printf("\nclient type: %T", client)
 }
 
-var cfg = weaviate.Config{
-	Host:   "localhost:8080",
-	Scheme: "http",
-}
+func CreateCollection(className string, description string, client *weaviate.Client) {
+	ctx := context.Background()
+	exists, err := client.Schema().ClassExistenceChecker().WithClassName(className).Do(ctx)
 
-func CreateCollection(name string, description string, client *weaviate.Client) {
+	// There's probably a more elegant way to do this error handling
+	if err != nil {
+		panic(err)
+	}
+	if exists == true {
+		fmt.Println("Collection already exists: ", className)
+		return
+	}
 
+	emptyClass := &models.Class{
+		Class:           className,
+		Description:     description,
+		Vectorizer:      "text2vec-openai", // Double check this. Probably wrong one
+		VectorIndexType: "hnsw",
+		Properties: []*models.Property{
+			{
+				Name:     "title",
+				DataType: schema.DataTypeText.PropString(),
+			},
+			{
+				Name:     "body",
+				DataType: schema.DataTypeText.PropString(),
+			},
+		},
+	}
+
+	err = client.Schema().ClassCreator().WithClass(emptyClass).Do(ctx)
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println("Class created:", className)
 }
