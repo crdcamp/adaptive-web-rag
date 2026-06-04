ea45a1da1665b2c3c91fab7a9d62b41575729ddb
Christian Camp <crdcamp@gmail.com>
2026-06-04 17:03:33 -0600

    Attempting to access nested keys in json

--- server/char-split-testing.go
+++ server/char-split-testing.go
@@ -2,29 +2,27 @@ package main
 
 import (
 	"encoding/json"
-	"fmt"
-	"io/ioutil"
 	"log"
+	"os"
 )
 
 func TestSplit() {
 	// Read `crawl_results.json`
-	content, err := ioutil.ReadFile("crawl_data/crawl_results.json")
+	content, err := os.ReadFile("crawl_data/crawl_results.json")
 	if err != nil {
 		log.Fatal("Error when opening file: ", err)
 	}
 
-	type CrawlData []struct {
-		Href    string `json:"href"`
-		Content string `json:"content"`
+	m := map[string]interface{}{}
+	err = json.Unmarshal(content, &m)
+	if err != nil {
+		panic(err)
 	}
 
-	var c []CrawlData
-	err = json.Unmarshal([]byte(content), &c)
-	if err != nil {
-		log.Fatal("Error unmarshaling into CrawlData: ", err)
-	}
-	fmt.Printf("Content: %v", string(content))
+	data, _ := json.MarshalIndent(m, "", " ")
+	//fmt.Println(string(data))
+
+	os.WriteFile("crawl_data/crawl_results_test.json", data, 0644)
 
 	// Text splitter
 	// textSplitter := charsplitter.New(
